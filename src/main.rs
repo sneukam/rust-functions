13b6faf07d4c5b606574630cd65dce6e08246895
Spencer Neukam <neukams@oregonstate.edu>
2023-09-03 17:38:10 -0400

    added comments and docstrings.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
+/*
+Program that plays with functions.
+Follwing The Book (rust book), chapter 3.
+*/
+
+
 fn main() {
     println!("Hello, world!");
 
+    // prints the values of the arguments
     another_function(5, 'Y');
 
+    // prints the result of five()
     let five_result: i32 = five();
     println!("function five() = {five_result}");
 }
 
+#[doc="Print the values aof x and h arguments."]
 fn another_function(x: i32, h: char) {
     println!("another_function()");
     println!("x = {x}, h = {h}");
 }
 
+#[doc="Return i32: 5"]
 fn five() -> i32 {
     5
 }
