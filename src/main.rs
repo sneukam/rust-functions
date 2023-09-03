063f9a6b8dcf5667fc21ecab78882224ea7a11db
Spencer Neukam <neukams@oregonstate.edu>
2023-09-03 17:23:02 -0400

    two function examples.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+
+    another_function(5, 'Y');
+
+    let five_result: i32 = five();
+    println!("function five() = {five_result}");
+}
+
+fn another_function(x: i32, h: char) {
+    println!("another_function()");
+    println!("x = {x}, h = {h}");
+}
+
+fn five() -> i32 {
+    5
 }
